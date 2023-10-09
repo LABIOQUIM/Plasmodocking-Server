2a514d91a28660a4db02c30e575a07b5bc627bb6
EduardoHernany <eduardohernany.pdm@gmail.com>
2023-10-09 07:57:01 -0400

    Plasmodocking - backend 9/10

---
+++ fiocruz/utils.py
@@ -0,0 +1,323 @@
+import glob
+import os
+import shutil
+import subprocess
+from .models import Arquivos_virtaulS, Macromoleculas_virtaulS, Testes, UserCustom, Macro_Prepare
+from rest_framework import routers, serializers, viewsets
+from django.http import FileResponse, HttpResponse, JsonResponse 
+from django.contrib.auth.models import User
+import json
+from .serializers import VS_Serializer
+import pandas as pd
+
+from django.conf import settings
+
+
+
+def extrair_energia_ligacao(caminho_arquivo):
+    # Variável para armazenar a energia de ligação
+    binding_energy = None
+
+    # Abra o arquivo em modo leitura
+    with open(caminho_arquivo, 'r') as arquivo:
+        # Percorra as linhas do arquivo
+        for linha in arquivo:
+            # Verifique se a linha contém a energia de ligação
+            if 'RANKING' in linha:
+                # Separe a linha por espaços em branco
+                dados = linha.split()
+
+                # Extraia a energia de ligação
+                binding_energy = dados[3]
+                run_energy = dados[2]
+                break
+
+    # Verifique se a energia de ligação foi encontrada
+    if binding_energy is not None:
+        return binding_energy,run_energy
+    else:
+        return "None"
+
+
+def converter_sdf_para_pdb(diretorio_sdf):
+    arquivos_sdf = glob.glob(os.path.join(diretorio_sdf, "*.sdf"))
+    if not arquivos_sdf:
+        return False
+
+    comando = ["obabel", "-isdf"] + arquivos_sdf + ["-opdb", "-O*.pdb"]
+    executar_comando(comando, diretorio_sdf)
+    return True
+
+def executar_comando(command, dir_path):
+
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=dir_path)
+    stdout, stderr = process.communicate()
+
+    if process.returncode != 0:
+        return HttpResponse(f"Ocorreu um erro: {stderr.decode()}")
+
+    
+    return True 
+
+
+def remover_arquivos_xml(dir_path, type_arquivo):
+    arquivos_xml = glob.glob(os.path.join(dir_path, type_arquivo))
+    for arquivo_xml in arquivos_xml:
+        try:
+            os.remove(arquivo_xml)
+        except Exception as e:
+            print(f"Erro ao remover o arquivo {arquivo_xml}: {e}")
+
+
+def listar_arquivos(diretorio):
+    command = ["ls"]
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=diretorio)
+    stdout, stderr = process.communicate()
+
+    if process.returncode != 0:
+        return False, f"Ocorreu um erro: {stderr.decode()}"
+    
+    # Obtém a saída do comando como uma string
+    saida = stdout.decode()
+    # Remove espaços em branco adicionais e quebras de linha
+    saida = saida.strip()
+    # Divide a string em uma lista de strings
+    ligantes_pdb = saida.split()
+
+    return ligantes_pdb
+
+def textfld():
+    return """
+label=C-affinity	# component label for variable 1
+label=A-affinity	# component label for variable 2
+label=N-affinity	# component label for variable 3
+label=NA-affinity	# component label for variable 4
+label=NS-affinity	# component label for variable 5
+label=OA-affinity	# component label for variable 6
+label=OS-affinity	# component label for variable 7
+label=SA-affinity	# component label for variable 8
+label=S-affinity	# component label for variable 9
+label=H-affinity	# component label for variable 10
+label=HD-affinity	# component label for variable 11
+label=HS-affinity	# component label for variable 1
+label=P-affinity	# component label for variable 2
+label=Br-affinity	# component label for variable 3
+label=BR-affinity	# component label for variable 4
+label=Ca-affinity	# component label for variable 5
+label=CA-affinity	# component label for variable 6
+label=Cl-affinity	# component label for variable 7
+label=CL-affinity	# component label for variable 8
+label=F-affinity	# component label for variable 9
+label=Fe-affinity	# component label for variable 10
+label=FE-affinity	# component label for variable 11
+label=I-affinity	# component label for variable 1
+label=Mg-affinity	# component label for variable 2
+label=MG-affinity	# component label for variable 3
+label=Mn-affinity	# component label for variable 4
+label=MN-affinity	# component label for variable 5
+label=Zn-affinity	# component label for variable 6
+label=ZN-affinity	# component label for variable 7
+label=He-affinity	# component label for variable 8
+label=Li-affinity	# component label for variable 9
+label=Be-affinity	# component label for variable 10
+label=B-affinity	# component label for variable 1
+label=Ne-affinity	# component label for variable 2
+label=Na-affinity	# component label for variable 3
+label=Al-affinity	# component label for variable 4
+label=Si-affinity	# component label for variable 5
+label=K-affinity	# component label for variable 6
+label=Sc-affinity	# component label for variable 7
+label=Ti-affinity	# component label for variable 8
+label=V-affinity	# component label for variable 9
+label=Co-affinity	# component label for variable 10
+label=Ni-affinity	# component label for variable 1
+label=Cu-affinity	# component label for variable 2
+label=Ga-affinity	# component label for variable 3
+label=Ge-affinity	# component label for variable 4
+label=As-affinity	# component label for variable 5
+label=Se-affinity	# component label for variable 6
+label=Kr-affinity	# component label for variable 7
+label=Rb-affinity	# component label for variable 8
+label=Sr-affinity	# component label for variable 9
+label=Y-affinity	# component label for variable 10
+label=Zr-affinity	# component label for variable 1
+label=Nb-affinity	# component label for variable 2
+label=Cr-affinity	# component label for variable 3
+label=Tc-affinity	# component label for variable 4
+label=Ru-affinity	# component label for variable 5
+label=Rh-affinity	# component label for variable 6
+label=Pd-affinity	# component label for variable 7
+label=Ag-affinity	# component label for variable 8
+label=Cd-affinity	# component label for variable 9
+label=In-affinity	# component label for variable 10
+label=Sn-affinity	# component label for variable 1
+label=Sb-affinity	# component label for variable 2
+label=Te-affinity	# component label for variable 3
+label=Xe-affinity	# component label for variable 4
+label=Cs-affinity	# component label for variable 5
+label=Ba-affinity	# component label for variable 6
+label=La-affinity	# component label for variable 7
+label=Ce-affinity	# component label for variable 8
+label=Pr-affinity	# component label for variable 9
+label=Nd-affinity	# component label for variable 10
+label=Pm-affinity	# component label for variable 1
+label=Sm-affinity	# component label for variable 2
+label=Eu-affinity	# component label for variable 3
+label=Gd-affinity	# component label for variable 4
+label=Tb-affinity	# component label for variable 5
+label=Dy-affinity	# component label for variable 6
+label=Ho-affinity	# component label for variable 7
+label=Er-affinity	# component label for variable 8
+label=Tm-affinity	# component label for variable 9
+label=Yb-affinity	# component label for variable 10
+label=Lu-affinity	# component label for variable 1
+label=Hf-affinity	# component label for variable 2
+label=Ta-affinity	# component label for variable 3
+label=W-affinity	# component label for variable 4
+label=Re-affinity	# component label for variable 5
+label=Os-affinity	# component label for variable 6
+label=Ir-affinity	# component label for variable 7
+label=Pt-affinity	# component label for variable 8
+label=Au-affinity	# component label for variable 9
+label=Hg-affinity	# component label for variable 10
+label=Tl-affinity	# component label for variable 1
+label=Pb-affinity	# component label for variable 2
+label=Bi-affinity	# component label for variable 3
+label=Po-affinity	# component label for variable 4
+label=At-affinity	# component label for variable 5
+label=Rn-affinity	# component label for variable 6
+label=Fr-affinity	# component label for variable 7
+label=Ra-affinity	# component label for variable 8
+label=Ac-affinity	# component label for variable 9
+label=Th-affinity	# component label for variable 10
+label=Pa-affinity	# component label for variable 1
+label=U-affinity	# component label for variable 2
+label=Np-affinity	# component label for variable 3
+label=Pu-affinity	# component label for variable 4
+label=Am-affinity	# component label for variable 5
+label=Cm-affinity	# component label for variable 6
+label=Bk-affinity	# component label for variable 7
+label=Cf-affinity	# component label for variable 8
+label=E-affinity	# component label for variable 9
+label=Fm-affinity	# component label for variable 10
+label=Electrostatics	# component label for variable 11
+label=Desolvation	# component label for variable 12
+#
+# location of affinity grid files and how to read them
+#
+variable 1 file=macro.C.map filetype=ascii skip=6
+variable 2 file=macro.A.map filetype=ascii skip=6
+variable 3 file=macro.N.map filetype=ascii skip=6
+variable 4 file=macro.NA.map filetype=ascii skip=6
+variable 5 file=macro.NS.map filetype=ascii skip=6
+variable 6 file=macro.OA.map filetype=ascii skip=6
+variable 7 file=macro.OS.map filetype=ascii skip=6
+variable 8 file=macro.SA.map filetype=ascii skip=6
+variable 9 file=macro.S.map filetype=ascii skip=6
+variable 10 file=macro.H.map filetype=ascii skip=6
+variable 11 file=macro.HD.map filetype=ascii skip=6
+variable 12 file=macro.HS.map filetype=ascii skip=6
+variable 13 file=macro.P.map filetype=ascii skip=6
+variable 14 file=macro.Br.map filetype=ascii skip=6
+variable 15 file=macro.BR.map filetype=ascii skip=6
+variable 16 file=macro.Ca.map filetype=ascii skip=6
+variable 17 file=macro.CA.map filetype=ascii skip=6
+variable 18 file=macro.Cl.map filetype=ascii skip=6
+variable 19 file=macro.CL.map filetype=ascii skip=6
+variable 20 file=macro.F.map filetype=ascii skip=6
+variable 21 file=macro.Fe.map filetype=ascii skip=6
+variable 22 file=macro.FE.map filetype=ascii skip=6
+variable 23 file=macro.I.map filetype=ascii skip=6
+variable 24 file=macro.Mg.map filetype=ascii skip=6
+variable 25 file=macro.MG.map filetype=ascii skip=6
+variable 26 file=macro.Mn.map filetype=ascii skip=6
+variable 27 file=macro.MN.map filetype=ascii skip=6
+variable 28 file=macro.Zn.map filetype=ascii skip=6
+variable 29 file=macro.ZN.map filetype=ascii skip=6
+variable 30 file=macro.He.map filetype=ascii skip=6
+variable 31 file=macro.Li.map filetype=ascii skip=6
+variable 32 file=macro.Be.map filetype=ascii skip=6
+variable 33 file=macro.B.map filetype=ascii skip=6
+variable 34 file=macro.Ne.map filetype=ascii skip=6
+variable 35 file=macro.Na.map filetype=ascii skip=6
+variable 36 file=macro.Al.map filetype=ascii skip=6
+variable 37 file=macro.Si.map filetype=ascii skip=6
+variable 38 file=macro.K.map filetype=ascii skip=6
+variable 39 file=macro.Sc.map filetype=ascii skip=6
+variable 40 file=macro.Ti.map filetype=ascii skip=6
+variable 41 file=macro.V.map filetype=ascii skip=6
+variable 42 file=macro.Co.map filetype=ascii skip=6
+variable 43 file=macro.Ni.map filetype=ascii skip=6
+variable 44 file=macro.Cu.map filetype=ascii skip=6
+variable 45 file=macro.Ga.map filetype=ascii skip=6
+variable 46 file=macro.Ge.map filetype=ascii skip=6
+variable 47 file=macro.As.map filetype=ascii skip=6
+variable 48 file=macro.Se.map filetype=ascii skip=6
+variable 49 file=macro.Kr.map filetype=ascii skip=6
+variable 50 file=macro.Rb.map filetype=ascii skip=6
+variable 51 file=macro.Sr.map filetype=ascii skip=6
+variable 52 file=macro.Y.map filetype=ascii skip=6
+variable 53 file=macro.Zr.map filetype=ascii skip=6
+variable 54 file=macro.Nb.map filetype=ascii skip=6
+variable 55 file=macro.Cr.map filetype=ascii skip=6
+variable 56 file=macro.Tc.map filetype=ascii skip=6
+variable 57 file=macro.Ru.map filetype=ascii skip=6
+variable 58 file=macro.Rh.map filetype=ascii skip=6
+variable 59 file=macro.Pd.map filetype=ascii skip=6
+variable 60 file=macro.Ag.map filetype=ascii skip=6
+variable 61 file=macro.Cd.map filetype=ascii skip=6
+variable 62 file=macro.In.map filetype=ascii skip=6
+variable 63 file=macro.Sn.map filetype=ascii skip=6
+variable 64 file=macro.Sb.map filetype=ascii skip=6
+variable 65 file=macro.Te.map filetype=ascii skip=6
+variable 66 file=macro.Xe.map filetype=ascii skip=6
+variable 67 file=macro.Cs.map filetype=ascii skip=6
+variable 68 file=macro.Ba.map filetype=ascii skip=6
+variable 69 file=macro.La.map filetype=ascii skip=6
+variable 70 file=macro.Ce.map filetype=ascii skip=6
+variable 71 file=macro.Pr.map filetype=ascii skip=6
+variable 72 file=macro.Nd.map filetype=ascii skip=6
+variable 73 file=macro.Pm.map filetype=ascii skip=6
+variable 74 file=macro.Sm.map filetype=ascii skip=6
+variable 75 file=macro.Eu.map filetype=ascii skip=6
+variable 76 file=macro.Gd.map filetype=ascii skip=6
+variable 77 file=macro.Tb.map filetype=ascii skip=6
+variable 78 file=macro.Dy.map filetype=ascii skip=6
+variable 79 file=macro.Ho.map filetype=ascii skip=6
+variable 80 file=macro.Er.map filetype=ascii skip=6
+variable 81 file=macro.Tm.map filetype=ascii skip=6
+variable 82 file=macro.Yb.map filetype=ascii skip=6
+variable 83 file=macro.Lu.map filetype=ascii skip=6
+variable 84 file=macro.Hf.map filetype=ascii skip=6
+variable 85 file=macro.Ta.map filetype=ascii skip=6
+variable 86 file=macro.W.map filetype=ascii skip=6
+variable 87 file=macro.Re.map filetype=ascii skip=6
+variable 88 file=macro.Os.map filetype=ascii skip=6
+variable 89 file=macro.Ir.map filetype=ascii skip=6
+variable 90 file=macro.Pt.map filetype=ascii skip=6
+variable 91 file=macro.Au.map filetype=ascii skip=6
+variable 92 file=macro.Hg.map filetype=ascii skip=6
+variable 93 file=macro.Tl.map filetype=ascii skip=6
+variable 94 file=macro.Pb.map filetype=ascii skip=6
+variable 95 file=macro.Bi.map filetype=ascii skip=6
+variable 96 file=macro.Po.map filetype=ascii skip=6
+variable 97 file=macro.At.map filetype=ascii skip=6
+variable 98 file=macro.Rn.map filetype=ascii skip=6
+variable 99 file=macro.Fr.map filetype=ascii skip=6
+variable 100 file=macro.Ra.map filetype=ascii skip=6
+variable 101 file=macro.Ac.map filetype=ascii skip=6
+variable 102 file=macro.Th.map filetype=ascii skip=6
+variable 103 file=macro.Pa.map filetype=ascii skip=6
+variable 104 file=macro.U.map filetype=ascii skip=6
+variable 105 file=macro.Np.map filetype=ascii skip=6
+variable 106 file=macro.Pu.map filetype=ascii skip=6
+variable 107 file=macro.Am.map filetype=ascii skip=6
+variable 108 file=macro.Cm.map filetype=ascii skip=6
+variable 109 file=macro.Bk.map filetype=ascii skip=6
+variable 110 file=macro.Cf.map filetype=ascii skip=6
+variable 111 file=macro.E.map filetype=ascii skip=6
+variable 112 file=macro.Fm.map filetype=ascii skip=6
+variable 113 file=macro.e.map filetype=ascii skip=6
+variable 114 file=macro.d.map filetype=ascii skip=6
+        """
+    return ligantes_pdb
